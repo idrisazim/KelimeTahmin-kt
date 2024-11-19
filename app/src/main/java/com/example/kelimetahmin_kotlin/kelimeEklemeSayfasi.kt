d7d87afe96a72f713d346c82443469218a4df640
Idris Azim <edrisazim25@gmail.com>
2024-11-19 18:12:14 +0300

    new activities

---
+++ app/src/main/java/com/example/kelimetahmin_kotlin/kelimeEklemeSayfasi.kt
@@ -0,0 +1,26 @@
+package com.example.kelimetahmin_kotlin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.kelimetahmin_kotlin.databinding.ActivityKelimeEklemeSayfasiBinding
+import com.example.kelimetahmin_kotlin.databinding.ActivityOyunSayfasiBinding
+
+class kelimeEklemeSayfasi : AppCompatActivity() {
+    private lateinit var binding: ActivityKelimeEklemeSayfasiBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityKelimeEklemeSayfasiBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
